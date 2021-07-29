30814d2d2e81353b25442331da2e7e40ff36f938
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-07-29 16:24:50 +0700

    Django Tutorial Part 3: Using models

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
 
-urlpatterns = [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]
+urlpatterns = []
